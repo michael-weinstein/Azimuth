ec62d908021244ec2da487ef379f4980e68fff8b
Michael Weinstein <michael.weinstein@ucla.edu>
2021-10-10 20:05:30 -0700

    Unlocking numpy and pandas to see if that fixes apparent conflicts

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='Azimuth',
       description=("Machine Learning-Based Predictive Modelling of CRISPR/Cas9 guide efficiency"),
       packages=["azimuth", "azimuth.features", "azimuth.models", "azimuth.tests"],
       package_data={'azimuth': ['saved_models/*.*']},
-      install_requires=['scipy==0.17.1', 'numpy==1.11.3', 'matplotlib==1.5.3', 'nose==1.3.7', 'scikit-learn>=0.17.1,<0.18', 'pandas==0.17.1', 'biopython==1.66'],
+      install_requires=['scipy==0.17.1', 'numpy', 'matplotlib==1.5.3', 'nose==1.3.7', 'scikit-learn>=0.17.1,<0.18', 'pandas', 'biopython==1.66'],
       license="BSD",
       # ext_modules=cythonize("ssk_cython.pyx"),
       )
